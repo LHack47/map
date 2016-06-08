5c16e0c03d1521b1f90fe855f78585d252d626b0
CodingMissDaisy <D-Saster@anniecannons.com>
2016-06-08 09:51:40 -0700

    Initial commit

---
+++ scripts/custom.js
@@ -0,0 +1,259 @@
+$(document).ready(function(){
+
+  $("#homePage-overlay-bar").hide();
+  $("#hamburger-overlay").hide();
+  $(".form-overlay").hide();
+  $(".dropdown-toggle").dropdown();
+
+  $("#dateRange").daterangepicker();
+  // $("#form-circumstances").show();
+    /* **VALUES***************************** */
+
+// homePage-overlay
+   $("#homePage-overlay-bar").on("click", function(){
+    $("#homePage-overlay-bar").hide();
+    $("#homePage-overlay").slideToggle("slow");
+  });
+/* END OF HOMEPAGE OVERLAY */
+
+/* HAMBURGER OVERLAY   HAMBURGER OVERLAY */
+  $(".glyphicon-menu-hamburger").on("click", function(){
+    $("#homePage-Elements").fadeOut("fast");
+    $("#pac-input").fadeOut("fast");
+    $("#homePage-overlay").fadeOut("fast")
+    $("#homePage-overlay-bar").fadeOut("fast");
+    $("#hamburger-overlay").show("slow");
+  });
+
+  $("#hamburger-overlay").on("click", function(){
+    $("#hamburger-overlay").fadeOut("fast");
+    $("#homePage-Elements").fadeIn("fast");
+    $("#pac-input").fadeIn("fast"); 
+    $("#homePage-overlay").fadeIn("fast");   
+  });
+   // $(".container").on("click", function){
+   //    $("")
+   // }
+
+   // $("#googleMap").one("click", function(){
+   //  $("#homePage-overlay").slideToggle("fast");
+   //  $("#homePage-overlay-bar").show("slow");
+   // });
+
+  $("#homePage-overlay > .glyphicon-chevron-down , #homePage-overlay > #addEvent").on("click", function(){
+    $("#transparent-overlay").hide();
+    $("#homePage-overlay").slideToggle("fast");
+    $("#homePage-overlay-bar").show("slow");
+  });
+
+  $("#transparent-overlay").on("click", function(){
+    $("#transparent-overlay").hide();
+    $("#homePage-overlay").slideToggle("fast");
+    $("#homePage-overlay-bar").show("slow");
+  });
+
+/* USER DENYS TO ADD MORE DETAILS */
+  $("#deny").on("click", function(){
+    $("#form-start-overlay").fadeOut("fast");
+    $("#form-thankYou").fadeIn("fast");
+  });   
+
+/* USER ACCEPTS TO ADD MORE DETAILS*/
+  $("#accept").on("click", function(){
+   $("#form-start-overlay").fadeOut("fast");
+   // $("#form-userInfo").fadeIn("slow");
+   $("#form-guidance").fadeIn("slow");
+  });
+
+/* FORM-GUIDANCE BUTTON */
+  $("#gotIt").on("click", function(){
+    $("#form-guidance").fadeOut("slow");
+    $("#form-userInfo").fadeIn("slow");
+  });
+
+/* USER INFO GLYPHICON CONTROL BUTTONS ***************************************************/
+  $(".userInfo-controls > .glyphicon-arrow-left").on("click", function(){
+    $("#form-userInfo").fadeOut("fast");
+    $("#form-start-overlay").fadeIn("fast");
+  });
+
+  $(".userInfo-controls > .glyphicon-floppy-saved").on("click", function(){
+    $("#form-userInfo").fadeOut("fast");
+    $("#homePage-Elements").fadeIn("fast");
+    $("#pac-input").fadeIn("fast");
+    $("#homePage-overlay-bar").fadeIn("slow");
+  });  
+
+  $(".userInfo-controls > .glyphicon-comment").on("click", function(){
+    $("#form-comments").fadeIn("slow");
+    
+      $("#form-comments > .comment-buttons > #comment-exit").on("click", function(){
+        $("#form-comments").fadeOut("slow");
+      });
+      $("#form-comments > .comment-buttons > #comment-save").on("click", function(){
+        $("#form-comments").fadeOut("slow");
+
+  userComment = $("#comments").val();
+  return userComment;
+
+         //userComment = $("#comments").val();
+        // firebase.child({user_page_comment: userComment});
+          //$("#comments").submit();
+      });
+  }); 
+
+  $(".userInfo-controls > .glyphicon-arrow-right").on("click", function(){
+    $("#form-userInfo").fadeOut("slow");
+    $("#form-attackerInfo").fadeIn("slow");
+    userGender = $("#userGender option:selected").val();
+    userAge = $("#userAge option:selected").val();
+  });
+
+
+/* ATTACKER INFO GLYPHICONS CONTROL BUTTONS ************************************************************************************** */  
+  $(".attackerInfo-controls > .glyphicon-arrow-left").on("click", function(){
+    $("#form-attackerInfo").fadeOut("slow");
+    $("#form-userInfo").fadeIn("slow");
+  });
+
+  $(".attackerInfo-controls > .glyphicon-floppy-saved").on("click", function(){
+    $("#form-attackerInfo").fadeOut("fast");
+    $("#homePage-Elements").fadeIn("fast");
+    $("#pac-input").fadeIn("fast");
+    $("#homePage-overlay-bar").fadeIn("slow");
+  }); 
+
+  $(".attackerInfo-controls > .glyphicon-comment").on("click", function(){
+    $("#form-comments").fadeIn("slow");
+      
+      $("#form-comments > .comment-buttons > #comment-exit").on("click", function(){
+        $("#form-comments").fadeOut("slow");
+      });
+       $("#form-comments > .comment-buttons > #comment-save").on("click", function(){
+        $("#form-comments").fadeOut("slow");
+         attackerComment = $("#comments").val();
+         //firebase.update({attacker_page_comment: attackerComment});
+          $("#comments").submit();
+      });
+  }); 
+
+  $(".attackerInfo-controls > .glyphicon-arrow-right").on("click", function(){
+    $("#form-attackerInfo").fadeOut("slow");
+    $("#form-circumstances").fadeIn("slow");
+    attackerGender = $("#attackerGender option:selected").val();
+    attackedBy = $("#attackedBy option:selected").val();
+    attackerRelationship = $("#attackerRelationship option:selected").val();
+  });
+
+  /* CIRCUMSTANCES GLYPHICON CONTROL BUTTONS
+  ************************************************************************************** */
+    $(".circumstances-controls > .glyphicon-arrow-left").on("click", function(){
+    $("#form-circumstances").fadeOut("slow");
+    $("#form-attackerInfo").fadeIn("slow");
+  });
+
+  $(".circumstances-controls > .glyphicon-floppy-saved").on("click", function(){
+    $("#form-circumstances").fadeOut("fast");
+    $("#homePage-Elements").fadeIn("fast");
+    $("#pac-input").fadeIn("fast");
+    $("#homePage-overlay-bar").fadeIn("slow");
+  }); 
+
+  $(".circumstances-controls > .glyphicon-comment").on("click", function(){
+    $("#form-comments").fadeIn("slow");
+     
+      $("#form-comments > .comment-buttons > #comment-exit").on("click", function(){
+        $("#form-comments").fadeOut("slow");
+      });
+       $("#form-comments > .comment-buttons > #comment-save").on("click", function(){
+        $("#form-comments").fadeOut("slow");
+         circumstancesComment = $("#comments").val();
+         //firebase.update({circumstances_page_comment: circumstancesComment});
+          $("#comments").submit();
+      });
+  });
+
+  $(".circumstances-controls > .glyphicon-arrow-right").on("click", function(){
+    $("#form-circumstances").fadeOut("slow");
+    $("#form-circumstances-2").fadeIn("slow");
+    date = document.getElementById("dateRange").value;
+    multipleAssaults = $("#multipleAssaults option:selected").val();
+  });
+
+/* FORM-CIRCUMSTANCES-2 GLYPHICON BUTTONS ******************************************** */
+  $(".circumstances-2-controls > .glyphicon-arrow-left").on("click", function(){
+    $("#form-circumstances-2").fadeOut("slow");
+    $("#form-circumstances").fadeIn("slow");
+  });
+
+  $(".circumstances-2-controls > .glyphicon-floppy-saved").on("click", function(){
+    $("#form-circumstances-2").fadeOut("fast");
+    $("#homePage-Elements").fadeIn("fast");
+    $("#pac-input").fadeIn("fast");
+    $("#homePage-overlay-bar").fadeIn("slow");
+  }); 
+
+  $(".circumstances-2-controls > .glyphicon-comment").on("click", function(){
+    $("#form-comments").fadeIn("slow");
+      
+      $("#form-comments > .comment-buttons > #comment-exit").on("click", function(){
+        $("#form-comments").fadeOut("slow");
+      });
+      $("#form-comments > .comment-buttons > #comment-save").on("click", function(){
+        $("#form-comments").fadeOut("slow");
+        circumstances2Comment = $("#comments").val();
+        //firebase.update({circumstances_page2_comment: circumstances2Comment});
+          $("#comments").submit();
+      });
+   });
+
+  $(".circumstances-2-controls > .glyphicon-arrow-right").on("click", function(){
+    $("#form-circumstances-2").fadeOut("slow");
+    $("#form-end").fadeIn("slow");
+    schoolCampus = $("#schoolCampus option:selected").val();
+    reported = $("#reported option:selected").val();
+  });
+
+/* FORM END PAGE ********************************************************************* */
+  $("#form-end > .btn-group-lg > .end-comment").on("click", function(){
+
+    $("#form-comments").fadeIn("slow");
+    
+      $("#form-comments > .comment-buttons > #comment-exit").on("click", function(){
+        $("#form-comments").fadeOut("slow");
+      });
+       $("#form-comments > .comment-buttons > #comment-save").on("click", function(){
+        $("#form-comments").fadeOut("slow");
+        lastComment = $("#comments").val();
+        //firebase.update({last_page_comment: lastComment});
+          $("#comments").submit();
+      });
+   });  
+
+  $("#form-end > .btn-group-lg > .end-restart").on("click", function(){
+    $("#form-end").fadeOut("fast");
+    $("#form-userInfo").fadeIn("fast");
+  });
+
+  $("#form-end > .btn-group-lg > .end-submit").on("click", function(){
+    $("#form-end").fadeOut("slow");
+    $("#form-thankYou").fadeIn("slow");
+  });  
+
+  $(".form-end-controls > .glyphicon-arrow-left").on("click", function(){
+    $("#form-end").fadeOut("slow");
+    $("#form-circumstances-2").fadeIn("fast");
+  });
+
+      $("#form-thankYou").on("click", function(){
+        $("#form-thankYou").fadeOut("slow");
+        $("#homePage-Elements").fadeIn("fast");
+        $("#pac-input").fadeIn("fast");
+        $("#homePage-overlay").hide();
+        $("#homePage-overlay-bar").fadeIn("slow");
+        
+      });   
+     
+}); /* DOCUMENT.READY(FUNCTION() */
+
+
---
+++ scripts/map.js
@@ -0,0 +1,157 @@
+var firebase = new Firebase("https://maptester.firebaseio.com/");
+
+/* SEARCH BOX + MAP*/
+ function initAutocomplete() {
+    var map = new google.maps.Map(document.getElementById('googleMap'), {
+      center: {lat: 37.734972, lng: -122.431297},
+      zoom: 12,
+      styles: [{"featureType":"all","elementType":"labels","stylers":[{"lightness":63},{"hue":"#ff0000"}]},{"featureType":"administrative","elementType":"all","stylers":[{"hue":"#000bff"},{"visibility":"on"}]},{"featureType":"administrative","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"administrative","elementType":"labels","stylers":[{"color":"#4a4a4a"},{"visibility":"on"}]},{"featureType":"administrative","elementType":"labels.text","stylers":[{"weight":"0.01"},{"color":"#727272"},{"visibility":"on"}]},{"featureType":"administrative.country","elementType":"labels","stylers":[{"color":"#ff0000"}]},{"featureType":"administrative.country","elementType":"labels.text","stylers":[{"color":"#ff0000"}]},{"featureType":"administrative.province","elementType":"geometry.fill","stylers":[{"visibility":"on"}]},{"featureType":"administrative.province","elementType":"labels.text","stylers":[{"color":"#545454"}]},{"featureType":"administrative.locality","elementType":"labels.text","stylers":[{"visibility":"on"},{"color":"#737373"}]},{"featureType":"administrative.neighborhood","elementType":"labels.text","stylers":[{"color":"#7c7c7c"},{"weight":"0.01"}]},{"featureType":"administrative.land_parcel","elementType":"labels.text","stylers":[{"color":"#404040"}]},{"featureType":"landscape","elementType":"all","stylers":[{"lightness":16},{"hue":"#ff001a"},{"saturation":-61}]},{"featureType":"poi","elementType":"labels.text","stylers":[{"color":"#828282"},{"weight":"0.01"}]},{"featureType":"poi.government","elementType":"labels.text","stylers":[{"color":"#4c4c4c"}]},{"featureType":"poi.park","elementType":"all","stylers":[{"hue":"#00ff91"}]},{"featureType":"poi.park","elementType":"labels.text","stylers":[{"color":"#7b7b7b"}]},{"featureType":"road","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels.text","stylers":[{"color":"#999999"},{"visibility":"on"},{"weight":"0.01"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"hue":"#ff0011"},{"lightness":53}]},{"featureType":"road.highway","elementType":"labels.text","stylers":[{"color":"#626262"}]},{"featureType":"transit","elementType":"labels.text","stylers":[{"color":"#676767"},{"weight":"0.01"}]},{"featureType":"water","elementType":"all","stylers":[{"hue":"#0055ff"}]}],
+          mapTypeControl: true,
+          mapTypeControlOptions: {
+            style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+            position: google.maps.ControlPosition.LEFT_BOTTOM
+           },
+          zoomControl: true,
+          zoomControlOptions: {
+            position: google.maps.ControlPosition.RIGHT_BOTTOM
+          },
+           streetViewControl: false
+    }); //styles
+
+
+  //info box when you click map
+  var questionString = "<div id='pinDrop-window'>" +
+    "<h3>Report a sexual assault at this location?</h3>"+
+    "<button type='button' class='btn btn-default btn-clicked' id='fireBase-no'>No</button>"+
+    "<button type='button' class='btn btn-default btn-clicked' id='fireBase-yes'>Yes</button>" +
+    "</div>"; // #pinDrop-window 
+
+  var questionWindow = new google.maps.InfoWindow({
+    content: questionString
+  }); //Call by:: questionWindow.open(map, marker);
+
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+      searchBox.setBounds(map.getBounds());
+    });
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function() {
+      var places = searchBox.getPlaces();
+
+      if (places.length == 0) {
+        return;
+      }
+      // For each place, get the icon, name and location.
+      var bounds = new google.maps.LatLngBounds();
+      places.forEach(function(place) {
+
+        // Create a marker for each place.
+        var marker = new google.maps.Marker({
+          position: place.geometry.location,
+          map: map,
+          fillColor: "green"
+        });
+
+        if (place.geometry.viewport) {
+          // Only geocodes have viewport.
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+
+        //question-window open
+        questionWindow.open(map, marker);
+  // SEND TO FIREBASE
+      $("#pinDrop-window > #fireBase-yes").on("click", function(e){
+        userLocation = {lat: place.geometry.location.lat(), lng: place.geometry.location.lng()};
+
+        $("#pinDrop-window").hide();
+        $("#homePage-Elements").fadeOut("fast");
+        $("#pac-input").fadeOut("fast");
+        $("#homePage-overlay-bar").fadeOut("fast");
+        $("#form-start-overlay").fadeIn("fast");
+        questionWindow.close();
+      });
+      $("#pinDrop-window > #fireBase-no").on("click", function(){
+        questionWindow.close();
+        //REMOVE MARKER WHEN USER SELECTS NO
+        marker.setVisible(false);
+      });
+    }); //place.forEach
+    map.fitBounds(bounds);
+      // console.log(bounds);
+    }); //searchBox.addListener
+
+
+
+     // Add marker on user click
+  map.addListener('click', function(e) {
+    var marker = new google.maps.Marker({
+    position: {lat: e.latLng.lat(), lng: e.latLng.lng()},
+    map: map
+    }); // var marker
+      questionWindow.open(map, marker);
+
+ //ADD PINDROP OVERLAY 
+      $("#pinDrop-window > #fireBase-yes").on("click", function(){
+       userLocation = {lat: e.latLng.lat(), lng: e.latLng.lng()};
+
+        $("#pinDrop-window").hide();
+        $("#homePage-Elements").fadeOut("fast");
+        $("#pac-input").fadeOut("fast");
+        $("#homePage-overlay-bar").fadeOut("fast");
+        $("#form-start-overlay").fadeIn("fast");
+        questionWindow.close();
+      });
+      $("#pinDrop-window > #fireBase-no").on("click", function(){
+        questionWindow.close();
+        //REMOVE MARKER WHEN USER SELECTS NO!!
+        marker.setVisible(false);
+      });
+    }); //map.addListener
+
+
+
+//DROP A MARKER WHEN MAP IS CLICKED 
+  firebase.on("child_added", function(snapshot, prevChildKey) {
+    // Get latitude and longitude from the cloud.
+    var newPosition = snapshot.val();
+
+    // Create a google.maps.LatLng object for the position of the marker.
+    var latLng = new google.maps.LatLng(newPosition.location.lat, newPosition.location.lng);
+
+    // Place a marker at that location.
+    var marker = new google.maps.Marker({
+      position: latLng,
+      map: map
+    });
+
+  //info box when you click a pin
+    var infoWindow = new google.maps.InfoWindow();
+
+     infoWindow.setContent(
+       '<div id="infoContent">'+
+       '<h3>' + 
+         location.userLocation + 
+       '</h3>' +
+       '<p>'+ userGender + ' , '+ 
+       userAge +' , '+ date + ' , '+ 
+       attackedBy + ' , ' + attackerGender + 
+      ' , ' + attackerRelationship + ' , ' + 
+      schoolCampus + ' , ' + reported +
+        '</p>'+
+      '</div>'
+      );
+
+    google.maps.event.addListener(marker, 'click', function() {
+      infoWindow.close(map, marker);
+      infoWindow.open(map, this);
+    }); // addListender(marker)
+
+  }); //firebase.on function
+} //initAutocomplete()
